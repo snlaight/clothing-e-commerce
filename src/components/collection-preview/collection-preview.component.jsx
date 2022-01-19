7c83b0b95639e67215611144315e4f2412f6eb68
snlaight <snlaight10@gmail.com>
2022-01-19 17:25:46 +0100

    moved our directory and shop date into its own reducer, created respective selectors, updated corresponding components with new redux flow directory/shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 
-
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
